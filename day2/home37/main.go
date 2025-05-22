c3d5697593310e50cae53dde720de7a0a318ce11
Denis Petukhov <p2houd@gmail.com>
2025-05-22 19:46:26 +0000

    fix homework day 2 add test

--- day2/home37/main.go
+++ day2/home37/main.go
@@ -5,31 +5,36 @@ import (
 	"unicode/utf8"
 )
 
-func main() {
-	var s string
+func findLongestWord(s string) (string, int) {
 	var maxRunes int
 	var curWord []rune
 	var maxWord string
 	var space int
-	// s, _ = bufio.NewReader(os.Stdin).ReadString('\n')
-	s = "1 22 333 4444 55555 666666"
+	s += " "
 	for _, v := range s {
 		// fmt.Println(i, v)
 		if v != ' ' {
 			curWord = append(curWord[:], v)
 			continue
 		}
+		// fmt.Println(maxWord)
+		if space > 4 {
+			break
+		}
 		// fmt.Println(string(curWord))
 		if utf8.RuneCountInString(string(curWord)) > maxRunes {
 			maxRunes = utf8.RuneCountInString(string(curWord))
 			maxWord = string(curWord)
 		}
-		// fmt.Println(maxWord)
-		if space > 5 {
-			break
-		}
 		curWord = nil
 		space++
 	}
+	return maxWord, maxRunes
+}
+
+func main() {
+	// s, _ = bufio.NewReader(os.Stdin).ReadString('\n')
+	s := "1 22 333 4444 55555 66666666 sd f"
+	maxWord, maxRunes := findLongestWord(s)
 	fmt.Printf("%v, %v\n", maxWord, maxRunes)
 }
